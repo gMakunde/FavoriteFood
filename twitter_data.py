b951073dfe5f720caa49f6e5ad3fc8774f4b2598
Ubuntu <ubuntu@ip-172-31-47-224.ec2.internal>
2020-02-09 23:51:47 +0000

    created a class that is used to get twitter json. with a method that gets json and a method that retrns random tweets from the json

---
+++ twitter_data.py
@@ -0,0 +1,74 @@
+import requests, requests_oauthlib
+import random
+
+class TwitterData:
+    """
+A class used to get twitter json
+
+...
+
+Attributes
+----------
+url: str
+a string containing a twitter api endpoint
+key: str
+twitter-dev api key
+secret_key: str
+twitter-dev api secret key
+token: str
+twitter-dev access token
+secret_token: str
+twitter-dev token secret
+
+Methods
+--------
+get_json()
+returns json body of the endpoint
+get_random_tweets(twit_json_body)
+returns a list containing 5 random tweets from the twit_json_body
+    """
+    def __init__(self, url, key, secret_key, token, secret_token):
+        """
+Parameters
+----------
+url: str
+a string containing a twitter api endpoint
+key: str
+twitter-dev api key
+secret_key: str
+twitter-dev api secret key
+token: str
+twitter-dev access token
+secret_token: str
+twitter-dev token secret
+"""
+        self.url = url
+        self.key = key
+        self.secret_key = secret_key
+        self.token = token
+        self.secret_token = secret_token
+    
+    def get_json(self):
+        """returns json body of the endpoint"""
+        twit_oauth = requests_oauthlib.OAuth1(
+            self.key,
+            self.secret_key,
+            self.token,
+            self.secret_token,
+        )
+        return requests.get(self.url, auth=twit_oauth).json()
+        
+    def get_random_tweets(self, twit_json_body):
+        """returns a list containing 5 random tweets from the twit_json_body
+
+Parameters
+----------
+twit_json_body: json
+the json body from a twitter endpoint
+"""
+        twit_random = random.randint(0, len(twit_json_body["statuses"])-5)
+        tweets = []
+        for i in range(5):
+            tweets.append(twit_json_body["statuses"][twit_random+i]['id'])
+        return tweets
+        
