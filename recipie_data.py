f9598be5742191dca9fe1ac70209a502273f0033
Ubuntu <ubuntu@ip-172-31-47-224.ec2.internal>
2020-02-10 00:19:05 +0000

    added docstrings that explain the class and its functions

--- recipie_data.py
+++ recipie_data.py
@@ -2,11 +2,47 @@ import requests
 
 
 class RecipieData:
-    def __init__(self, url, key):
-        self.url = url
+    """
+A class used to return recipie json from multiple queries
+
+...
+
+Attributes
+----------
+url: str
+a string containing spoonacular Get Recipe Information Bulk api endpoint. 
+the default endpoint has no ids.
+key: str
+spoonacular api key
+
+Methods
+--------
+group_queries()
+appends every result's id to the url
+get_json()
+returns json body of the endpoint
+"""
+    def __init__(self, key):
+        """
+Parameters
+----------
+url: str
+a string containing spoonacular Get Recipe Information Bulk api endpoint. 
+the default endpoint has no ids.
+key: str
+spoonacular api key
+"""
+        self.url = "https://api.spoonacular.com/recipes/informationBulk?ids="
         self.key = key
     
     def group_queries(self, url_list):
+        """appends every result's id to the url.
+
+Parameters
+-----------
+url_list: list
+list of strings that contain Search Recipes endpoints from spoonacular api
+"""
         responses = []
         for urls in url_list:
             responses.append(requests.get(urls + self.key).json())
@@ -16,6 +52,13 @@ class RecipieData:
                 self.url += str(response["results"][i]["id"]) + ","
 
     def get_json(self):
+        """returns json body of the endpoint
+
+Returns
+--------
+json
+json body that contains recipie information from one or more queries
+"""
         return requests.get(self.url + self.key).json() 
 
 
